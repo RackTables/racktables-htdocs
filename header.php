14aa6896bb20852384e1293c505ea08cfe57bbc3
Denis Ovsienko <denis@ovsienko.info>
2017-05-15 13:53:46 +0100

    save changes made for 0.20.13

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
-$lastrelease = '0.20.12';
-$demorelease = '0.20.12';
+$lastrelease = '0.20.13';
+$demorelease = '0.20.13';
 if (!isset($bodyId))
 	$bodyId = 'IndexPage';
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
