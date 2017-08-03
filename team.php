c7994089ca4b1dc5684700dd231547f461e182a9
Denis Ovsienko <denis@ovsienko.info>
2017-08-03 16:19:44 +0100

    update responsibilities in team.php

--- team.php
+++ team.php
@@ -6,21 +6,21 @@ include 'header.php';
 <h2 id="pilot">Denis Ovsienko</h2>
   <b>IRC Nick:</b> pilot<br>
   <b>Location:</b> Maidstone, Kent, United Kingdom<br>
-  <b>RackTables Responsibilities:</b> developer<br>
+  <b>RackTables Responsibilities:</b> developer, release manager<br>
   <b>Component Responsibilities:</b> 802.1Q VLANs<br>
 <br><hr><br>
 
 <h2 id="adoom42">Aaron Dummer</h2>
   <b>IRC Nick:</b> adoom42<br>
   <b>Location:</b> San Francisco, USA<br>
-  <b>RackTables Responsibilities:</b> Bug Tracker and Wiki software administrator, treasurer<br>
+  <b>RackTables Responsibilities:</b> developer, bug tracker and wiki software administrator, treasurer<br>
   <b>Component Responsibilities:</b> SNMP, VMs/containers<br>
 <br><hr><br>
 
 <h2 id="andriyanov">Alexey Andriyanov</h2>
   <b>IRC Nick:</b> N/A<br>
   <b>Location:</b> Moscow, Russia<br>
-  <b>RackTables Responsibilities:</b> developer, release manager<br>
+  <b>RackTables Responsibilities:</b> developer<br>
   <b>Component Responsibilities:</b> IPv4/IPv6/SLB, JavaScript<br>
 <br><hr><br>
 
