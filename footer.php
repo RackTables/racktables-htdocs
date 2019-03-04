41b4b172e3ae531c833c4607b3a95f659b58c681
Denis Ovsienko <denis@ovsienko.info>
2019-03-04 22:08:51 +0000

    use HTTPS for the source code mirror

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 		<ul>
 			<li><a href="https://wiki.racktables.org/">Wiki</a></li>
 			<li><a href="https://github.com/RackTables/racktables">Source code</a> <img width=16 height=16 src='/img/ext-link.png'>
-				<small>(<a href="http://code.racktables.org/">mirror</a>)</small></li>
+				<small>(<a href="https://code.racktables.org/">mirror</a>)</small></li>
 			<li><a href='https://bugs.racktables.org/'>Bug tracker</a></li>
 			<li><a href='https://bugs.racktables.org/roadmap_page.php'>Roadmap</a></li>
 			<li><a href="https://sourceforge.net/donate/index.php?group_id=293827"><img src="/img/sf-project-support.jpeg" width="88" height="32" border="0" alt="Support This Project" /></a>
