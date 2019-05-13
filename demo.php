0bc1f76073bb3730964d0704ea4b06b8c9858098
Denis Ovsienko <denis@ovsienko.info>
2019-05-13 18:27:47 +0100

    replace some passive voice

--- demo.php
+++ demo.php
@@ -3,7 +3,7 @@ $bodyId = 'DemoPage';
 include 'header.php';
 ?>
 <div id="Content">
-	<p>The demo database is reset to the initial state by a cron script, don't be afraid to break anything there and don't input any valuable data, it will be lost. Log in with full privileges using <strong>username: <em>admin</em></strong> and <strong>password: <em>admin</em></strong>. Have fun!</p>
+	<p>A cron job resets the demo database to the initial state, hence do not be afraid to break anything there. On the other hand, do not input any data that you are not ready to lose and/or to share with the world. Log in with full privileges using <strong>username: <em>admin</em></strong> and <strong>password: <em>admin</em></strong>. Have fun!</p>
 	<p><a href="http://demo.racktables.org/">Release <?php echo $demorelease;?> demo<img width=16 height=16 src='/img/ext-link.png'></a></p>
 </div>
 <?php include 'footer.php';?>
