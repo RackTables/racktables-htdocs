c19c110edde7ae0db128abd2a65dbc4e53924ec5
Denis Ovsienko <denis@ovsienko.info>
2020-04-21 10:43:47 +0100

    simplify and update team.php

--- team.php
+++ team.php
@@ -5,21 +5,19 @@ include 'header.php';
 <div id="Content">
 <h2 id="pilot">Denis Ovsienko</h2>
   <b>IRC Nick:</b> pilot<br>
-  <b>Location:</b> Maidstone, Kent, United Kingdom<br>
-  <b>RackTables Responsibilities:</b> developer, release manager<br>
-  <b>Component Responsibilities:</b> 802.1Q VLANs<br>
+  <b>Location:</b> Edinburgh, United Kingdom<br>
+  <b>Responsibilities:</b> developer<br>
 <br><hr><br>
 
 <h2 id="adoom42">Aaron Dummer</h2>
   <b>IRC Nick:</b> adoom42<br>
   <b>Location:</b> San Francisco, USA<br>
-  <b>RackTables Responsibilities:</b> developer, bug tracker and wiki software administrator, treasurer<br>
-  <b>Component Responsibilities:</b> SNMP, VMs/containers<br>
+  <b>Responsibilities:</b> web-master and treasurer<br>
 <br><hr><br>
 
 <h2 id=alaunay">Arnaud Launay</h2>
   <b>IRC Nick:</b> alaunay<br>
   <b>Location:</b> France<br>
-  <b>RackTables Responsibilities:</b> hosting and systems administration for the bug tracker, wiki and demo<br>
+  <b>Responsibilities:</b> systems administrator<br>
 </div>
 <?php include 'footer.php';?>
