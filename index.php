94cf5fec3cf4a48368bc1c10ee384e066f837bed
Denis Ovsienko <denis@ovsienko.info>
2020-09-04 14:45:28 +0100

    fix remaining XHTML errors in index.php
    Line 33, Column 10: ID "Download" already defined
    Line 32, Column 12: ID "Download" first defined here
    Line 34, Column 120: ID "DownloadButton" already defined
    Line 32, Column 49: ID "DownloadButton" first defined here

--- index.php
+++ index.php
@@ -8,14 +8,14 @@ require 'header.php';
 	<div id="MainScreenshot"><img src="img/screenshots/shot-indexpage.png" width="471" height="313" alt="index page screenshot" /></div>
 	<div id="MainText">
 		<p>RackTables is a nifty and robust solution for datacenter and server room asset management. It helps document hardware assets, network addresses, space in racks, networks configuration and much much more!</p>
-		<div id="Download"><h3>Check the</h3><div id="DownloadButton"><a href="demo.php">Live demo (<?php echo $demorelease; ?>)</a></div></div>
-<div id="Download">
+		<div class="Download"><h3>Check the</h3><div class="DownloadButton"><a href="demo.php">Live demo (<?php echo $demorelease; ?>)</a></div></div>
+<div class="Download">
 <?php if ( isset ($currentrelease) && $currentrelease !== $lastrelease) { ?>
-		<h3>The current release<?php echo EXTLINKIMG; ?></h3><div id="DownloadButton">
+		<h3>The current release<?php echo EXTLINKIMG; ?></h3><div class="DownloadButton">
 		<a href="https://sourceforge.net/projects/racktables/files/RackTables-<?php echo $currentrelease;?>.tar.gz/download">RackTables <?php echo $currentrelease;?>
 		</a></div><br/>
 <?php  } ?>
-		<h3>The latest stable release<?php echo EXTLINKIMG; ?></h3><div id="DownloadButton">
+		<h3>The latest stable release<?php echo EXTLINKIMG; ?></h3><div class="DownloadButton">
 		<a href="https://sourceforge.net/projects/racktables/files/RackTables-<?php echo $lastrelease;?>.tar.gz/download">RackTables <?php echo $lastrelease;?>
 		</a></div></div>
 	</div>
