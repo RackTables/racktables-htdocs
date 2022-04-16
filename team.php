f4d6b74e7e7e3408010f772b4cd030857048501d
Denis Ovsienko <denis@ovsienko.info>
2022-04-16 22:25:29 +0100

    team.php: tell the country only

--- team.php
+++ team.php
@@ -5,14 +5,14 @@ require 'header.php';
 <div id="Content">
 <h2 id="pilot">Denis Ovsienko</h2>
   <b>IRC Nick:</b> denis<br/>
-  <b>Location:</b> Edinburgh, United Kingdom<br/>
+  <b>Location:</b> United Kingdom<br/>
   <b>Responsibilities:</b> developer<br/>
 <br/>
 <hr/>
 <br/>
 <h2 id="adoom42">Aaron Dummer</h2>
   <b>IRC Nick:</b> adoom42<br/>
-  <b>Location:</b> San Francisco, USA<br/>
+  <b>Location:</b> USA<br/>
   <b>Responsibilities:</b> web-master and treasurer<br/>
 <br/>
 <hr/>
